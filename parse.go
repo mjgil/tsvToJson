c9be97dbf921f7e5bb3194bbf3f07a82189ca0d1
Malcom Gilbert <malcomgilbert@gmail.com>
2014-10-17 01:13:51 -0400

    reading lines

--- parse.go
+++ parse.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"bufio"
+		"os"
+		"log"
+		)
 
 func main() {
+
 	fmt.Println("Hi There")
+	file, err := os.Open("test.tsv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
