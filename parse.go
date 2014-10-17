ec4beff2c59c5ac6c0c14fcda98ece1503c14076
Malcom Gilbert <malcomgilbert@gmail.com>
2014-10-17 02:10:44 -0400

    camelCase first line"

--- parse.go
+++ parse.go
@@ -6,7 +6,17 @@ import (
 		"os"
 		"log"
 		"regexp"
+		"strings"
 		)
+
+func toCamelCase(str string) (string) {
+	lower := strings.ToLower(str)
+	regex := regexp.MustCompile("\\s+(\\w)")
+	capit := regex.ReplaceAllStringFunc(lower, func(m string) (string) {
+		return strings.TrimSpace(strings.ToUpper(m))
+	})
+	return capit
+}
 
 func main() {
 
@@ -19,10 +29,21 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	first := 1
 	for scanner.Scan() {
 		scanText := scanner.Text()
 		regex := regexp.MustCompile("\t")
 		splitted := regex.Split(scanText, -1)
 		fmt.Println(splitted, len(splitted))
+		if first == 1 {
+			fmt.Println("first")
+			first = 0
+			for _, elem := range splitted {
+				fmt.Println(elem)
+				fmt.Println(toCamelCase(elem))
+			}
+		} else {
+			fmt.Println("else")
+		}
 	}
 }
